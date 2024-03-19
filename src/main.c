34dce2ae70a8d0b2a838f9963887128929fc6df1
SHJ984 <dokdae0624@naver.com>
2024-03-19 11:32:56 +0900

    y

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@ int block[4][4]={
     {1,0,0,0},
     {1,0,0,0}};
 
+int tetris_table[21][10];
+
 void display_tetris()
 {
     system("clear");
@@ -15,14 +17,30 @@ void display_tetris()
             if(block){
             printf("o ");
             {
-                else{        
-            printf("* ");
-        }
-    }
+                else {        
+                    printf("* ");
+                }
+                }
+        printf("\n");
             }
         }
     }      
-    printf("Tetris\n");
+    printf("\n\n");
+    printf("Tetris Table\n");
+    for (int i = 2; i < 21; i++){
+        printf("\t");
+        for (int j = 0; j < 10; j++){
+            if (j == 0 || j == 9){
+                printf("|");
+                }else if (i == 20 && (j > 1 || j < 9)){
+                    printf("-");
+            }else if (tetris_table[j][i] == 0){
+                printf("-");
+            }else if (tetris_table[j][i] == 1) {
+                
+            }
+        }
+    }
 }
 
 int display_menu()
