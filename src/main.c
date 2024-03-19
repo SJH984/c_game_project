e9619d27061cc92f75d9765c89dff09dd7e81fc5
SHJ984 <dokdae0624@naver.com>
2024-03-19 11:13:20 +0900

    kkgkg

--- src/main.c
+++ src/main.c
@@ -1,7 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+int block[4][4]={
+    {1,0,0,0},
+    {1,0,0,0},
+    {1,0,0,0},
+    {1,0,0,0}};
+
+void display_tetris()
+{
+    system("clear");
+    printf("Next Block\n");
+    for (int t = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            if(block){
+            printf("o ");
+            {
+                else{        
+            printf("* ");
+        }
+    }
+            }
+        }
+    }      
+    printf("Tetris\n");
+}
+
+int display_menu()
+{
+    int menu;
+
+    system("clear");
+
+    printf("tetris SJH\n");
+    printf("-------------------\n");
+    printf("\t\tGAME MENU\n");
+    printf("--------------------\n");
+    printf("1:Game Start\n");
+    printf("2.Search history\n");
+    printf("3.Rescord Output\n");
+    printf("4.Quit\n");
+
+    scanf("%d", &menu);
+    return menu;
+
+}
 
 int main()
 {
-    printf("Hello, World\n");
+    int menu = 1;
+    menu = display_menu();
+    switch (menu)
+    {
+    case 1:
+        printf("Game Start\n");
+        displat tetris();
+        break;
+    case 2:
+        printf("Search history\n");
+        break;
+    case 3:
+        printf("Record Output\n");
+        break;
+    case 4:
+       printf("Quit\n");
+       break;
+    }
+    
     return 0;
 }
